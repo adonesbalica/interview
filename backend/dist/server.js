5faa5957f5fb6cdfb1c296e66e72d2ef457f1d03
Adones Balica <adonesbalica@gmail.com>
2026-07-19 17:52:27 -0300

    remove unused backend files and update package.json for build and start scripts

--- backend/dist/server.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { createApp } from "./app.js";
-const PORT = Number(process.env["PORT"] ?? 3000);
-const app = createApp();
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
-//# sourceMappingURL=server.js.map
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,29 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "node:path";
 
 const PORT = Number(ENV.PORT ?? 3000);
 
 const app = express();
 
-app.get("/", (req, res) => {
+const __dirname = path.resolve();
+
+app.get("/health", (req, res) => {
   res.status(200).json({ message: "Ok" });
 });
+
+app.get("/books", (req, res) => {
+  res.status(200).json({ message: "Books Endpoint" });
+});
+
+// make ready for deploy
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
